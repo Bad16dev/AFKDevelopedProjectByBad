85c96b622ed6e00d5fb2608f002178dc19cbd766
Bad Ro <71897530+Bad16dev@users.noreply.github.com>
2022-07-15 04:29:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# `AFK Developed Project`
+***
+### This Self Bot Is Maked By Bad
+__My Discord :__ Odqin Hope.#3890 | Ho, Badro#8500
+#### Discord Accounts :
+![Discord](https://discord.c99.nl/widget/theme-5/986064977062400060.png)
+![Discord](https://discord.c99.nl/widget/theme-5/810262257447469116.png)
+
+> You Can Always Support Me By Flow Me In My **Social Media Accounts**.
+
+[My Channel](https://www.youtube.com/channel/UCKh6D-uY87Bb0y0w7XqUlzQ) | [My Instagram](https://www.instagram.com/bqdro_16/) | [My Facebook](https://www.facebook.com/B4dRo)
+
+## [Flou Codes.](https://discord.gg/CkXfzjYVJw)
+<a href="https://discord.com/invite/CkXfzjYVJw"><img src="https://discord.com/api/guilds/934493669191462982/widget.png?style=banner2"></a>
+
+# Features
+
+- Slash Commands  
+- Pause Command
+- Resume Command
+- Giveaway Active List Command
+- Role To Participate 
+- Join Server To Participate
+- Bonus Role Of Luck
+- And More...
+
+# Credits
+
+> [Click Here](https://bit.ly/3wXa5lB) 
+
+> All Copyrights Reserved ©️ 
+
+ 
+
---
+++ index.js
@@ -0,0 +1,47 @@
+///Hala Madrid
+const express = require("express");
+const app = express();
+const Discord = require('discord.js-v11-fixes');
+const ffmpeg = require('ffmpeg');
+const ffmpegstatic = require('ffmpeg-static');
+const cmd = require("node-cmd");
+const client = new Discord.Client();
+const config = require('./config.json');
+const prefix = config.prefix;
+const id = config.id;
+///Ready
+client.on("ready", () => {
+console.log(`Log In : ${client.user.tag}..`);
+app.listen(() => console.log('Bad Start Go Go'));
+app.get('/', (req, res) => {
+res.send('<center><h1><u>Ready<u/><h1/>')
+});
+ });
+///Code
+//Join
+client.on('message', message => {
+         let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1).join(" ");
+if (id.includes(message.author.id)) 
+{
+    if(message.content.startsWith(prefix + "join")) {
+  console.log("Joining..."); 
+  let hope = client.channels.get(`${args}`)
+  
+  hope.join();
+}
+}})
+//Leave
+client.on('message', message => {
+  let messageArray = message.content.split(" ");
+    let args = messageArray.slice(1).join(" ");
+  if (id.includes(message.author.id)) 
+{
+    if(message.content.startsWith(prefix + "leave")) {
+  console.log("Leaving..."); 
+  let hope = client.channels.get(`${args}`)
+  hope.leave();
+}
+}})
+///Login
+client.login(process.env.token);
